a6ac35a5415c4e08b08ff76df12229eab929ce24
deveshsingh-rn <devesh.singh@fundenflo.com>
2026-08-20 17:43:24 +0530

    ui

--- screens/splashscreen.tsx
+++ screens/splashscreen.tsx
@@ -142,7 +142,7 @@ const styles = StyleSheet.create({
   },
   splashWelcomeFooter: {
     alignItems: "center",
-    paddingBottom: 36,
+    // paddingBottom: 36,
     paddingHorizontal: 26,
   },
   splashWelcomeSubtitle: {
